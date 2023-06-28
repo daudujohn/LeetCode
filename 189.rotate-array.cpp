2ddc07e0820b58a3fe431bf88fc3a3a68aaae33d
daudujohn <daudujoe@gmail.com>
2023-06-29 00:47:54 +0100

    Accepted 38/38 cases passed (29 ms) Your runtime beats 88.78 % of cpp submissions Your memory usage beats 65.7 % of cpp submissions (25 MB)

--- 189.rotate-array.cpp
+++ 189.rotate-array.cpp
@@ -8,15 +8,26 @@
 class Solution
 {
 public:
+    void reverse(vector<int> &nums, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
     void rotate(vector<int> &nums, int k)
     {
-        int last_element = nums.size() - 1;
-        for (int i = 0; i < k; i++)
+        if (k > nums.size())
         {
-            int num = nums[last_element];
-            nums.pop_back();
-            nums.insert(nums.begin(), num);
+            k = k % nums.size();
         }
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
     }
 };
 // @lc code=end
